c01ca847b827afabaf364e42108718767af5aa77
Rachel Gapasin <gapasinrachel@gmail.com>
2023-10-23 01:48:22 -0400

    Add search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="text-center">Wordist</h1>
+        <h1 className="text-center">[Wordist] Dictionary</h1>
         <h3>noun</h3>
         <p>Someone who is passionate about words, language, or vocabulary</p>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center mt-3">Coded by Rachel Gapasin 👩🏽‍💻</footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyWord, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange}/>
+      </form>
+    </div>
+  );
+}
