5083c0da5adc076041b823ea8fb6464b48d06626
Rachel Gapasin <gapasinrachel@gmail.com>
2023-10-23 01:01:27 -0400

    Initialize using Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary</header>
+      
+    </div>
+  );
+}
+
+export default App;
