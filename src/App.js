d1ee933f4e9dd30e60b36bd154f4fa06340480b2
Rachel Gapasin <gapasinrachel@gmail.com>
2023-10-23 01:30:51 -0400

    Add header

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
-      
+      <header className="App-header">
+        <h1 className="text-center">Wordist</h1>
+        <h3>noun</h3>
+        <p>Someone who is passionate about words, language, or vocabulary</p>
+      </header>
     </div>
   );
 }
